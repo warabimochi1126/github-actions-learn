aae0ceeb52733cc82ea7781b407b4e4b09ce3399
warabimochi <satomi920003@yahoo.co.jp>
2024-09-10 16:24:29 +0900

    ふぁいるへんこう

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,10 +4,13 @@ import { danger, fail } from "danger";
 function checkPRTitle() {
   const prTitle = danger.github.pr.title;
 
+  console.log(prTitle);
+
   const regex = /^(feat|fix|docs|refactor|test|ci)\([^)]+\):\s*.+/i;
   if (prTitle.match(regex)) {
     fail("PRタイトルがConventional Commitsに準拠していません。");
   }
+  fail("絶対落とす");
 }
 
 checkPRTitle();
